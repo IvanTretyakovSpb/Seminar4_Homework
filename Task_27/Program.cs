0909159b75c2ced18a958e5dec2963440eb1e43f
Tretyakov Ivan <timmitarh@mail.ru>
2023-03-07 17:06:20 +0300

    Completed task #27

---
+++ Task_27/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Input
+Console.Clear();
+int userNumber = GetNumberFromUser("Введите целое положительное число: ", "Ошибка ввода!");
+// Logic
+int resultSumDigits = GetResultSumDigits(userNumber);
+// Output
+Console.WriteLine($"{userNumber} -> {resultSumDigits}");
+
+/////////////////////////// опеределение методов
+int GetResultSumDigits(int number)
+{
+    int result = 0;
+    while(number > 0)
+    {
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect == true && userNumber > 0)
+        {
+            return userNumber;
+        }
+        Console.WriteLine(errorMessage);
+    }
+}
+
